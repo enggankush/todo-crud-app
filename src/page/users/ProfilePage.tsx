4c7acd23875d89819d254ae7940f4c5a054e3da8
Ankush <livemail.ankush@gmail.com>
2025-09-15 11:15:38 +0530

    create profile page

--- src/page/users/ProfilePage.tsx
+++ src/page/users/ProfilePage.tsx
@@ -1,13 +1,23 @@
-import { Box, Button, Avatar, Card, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Header from "../../components/nav/Header";
 import { useEffect, useState } from "react";
 import { getUser, isLoggedIn } from "../../api/api.service";
 import { Navigate } from "react-router-dom";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 
 const ProfilePage: React.FC = () => {
   if (!isLoggedIn) {
     return <Navigate to="/login" />;
   }
+
+  const [isEditing, setIsEditing] = useState(false);
 
   const [user, setUser] = useState<User>({
     name: "",
@@ -15,47 +25,155 @@ const ProfilePage: React.FC = () => {
     mobile: "",
     email: "",
     _id: null,
+    image: "",
+    address: "",
   });
+
+  const [formData, setFormData] = useState(user);
 
   useEffect(() => {
     const user = getUser();
     if (user) {
       setUser(user);
+      setFormData(user);
     }
   }, []);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+  const handleSaveClick = () => {
+    setUser(formData);
+    setIsEditing(false);
+    alert("✅ Profile Updated !");
+  };
+  const handleCancelClick = () => {
+    setIsEditing(false);
+    setFormData(user);
+  };
+
+  // const handleButton = () =>{
+
+  // }
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
   return (
     <Box>
       <Header />
-      <Box sx={{ display: "flex", justifyContent: "center", mt: 8 }}>
-        <Card elevation={4} sx={cardStyle}>
-          <Avatar sx={avatarStyle}>
-            {user.name ? user.name.charAt(0).toUpperCase() : "G"}
-          </Avatar>
-          <Typography>
-            <b>My Profile</b>
-          </Typography>
-          <Box sx={{ textAlign: "left", mt: 3 }}>
-            <Typography variant="body1" sx={{ mb: 1 }}>
-              <b>Name:</b> {user.name || "-"}
-            </Typography>
-            <Typography variant="body1" sx={{ mb: 1 }}>
-              <b>DOB:</b> {user.dob || "-"}
-            </Typography>
-            <Typography variant="body1" sx={{ mb: 1 }}>
-              <b>Mobile:</b> {user.mobile || "-"}
-            </Typography>
-            <Typography variant="body1" sx={{ mb: 2 }}>
-              <b>Email:</b> {user.email || "-"}
-            </Typography>
+
+      <Box sx={{ display: "flex", justifyContent: "center", m: 10 }}>
+        <Card sx={cardStyle}>
+          <Box sx={imageBoxStyle}>
+            <img
+              src={user.image || "../../src/assets/images/ank.jpg"}
+              alt={user.name}
+              style={{
+                width: "100%",
+                height: "100%",
+                borderRadius: "8px",
+                objectFit: "cover",
+              }}
+            />
           </Box>
-          <Button
-            sx={buttonStyle}
-            variant="contained"
-            color="primary"
-            fullWidth
-          >
-            Profile
-          </Button>
+
+          <CardContent sx={{ flex: "1" }}>
+            {isEditing ? (
+              <>
+                <TextField
+                  name="name"
+                  label="Name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  fullWidth
+                  sx={{ mb: 2 }}
+                />
+                <TextField
+                  name="dob"
+                  label="DOB"
+                  value={formData.dob}
+                  onChange={handleChange}
+                  fullWidth
+                  sx={{ mb: 2 }}
+                />
+                <TextField
+                  name="mobile"
+                  label="Mobile"
+                  value={formData.mobile}
+                  onChange={handleChange}
+                  fullWidth
+                  sx={{ mb: 2 }}
+                />
+                <TextField
+                  name="email"
+                  label="Email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  fullWidth
+                  disabled
+                  sx={{ mb: 2 }}
+                />
+                <TextField
+                  name="address"
+                  label="Address"
+                  value={formData.address}
+                  onChange={handleChange}
+                  fullWidth
+                  sx={{ mb: 2 }}
+                />
+
+                <Box sx={{ display: "flex", gap: 2 }}>
+                  <Button
+                    sx={buttonStyle}
+                    variant="contained"
+                    color="primary"
+                    onClick={handleSaveClick}
+                  >
+                    Save
+                  </Button>
+                  <Button
+                    sx={buttonStyle}
+                    variant="outlined"
+                    color="secondary"
+                    onClick={handleCancelClick}
+                  >
+                    Cancel
+                  </Button>
+                </Box>
+              </>
+            ) : (
+              <>
+                <Typography variant="body1" sx={{ mb: 2 }}>
+                  <b>Name:</b> {user.name || "-"}
+                </Typography>
+                <Typography variant="body1" sx={{ mb: 2 }}>
+                  <b>DOB:</b> {user.dob || "-"}
+                </Typography>
+                <Typography variant="body1" sx={{ mb: 2 }}>
+                  <b>Mobile:</b> {user.mobile || "-"}
+                </Typography>
+                <Typography variant="body1" sx={{ mb: 2 }}>
+                  <b>Email:</b> {user.email || "-"}
+                </Typography>
+                <Typography variant="body1" sx={{ mb: 2 }}>
+                  <b>Address:</b> {user.address || "-"}
+                </Typography>
+                <Button
+                  sx={buttonStyle}
+                  variant="outlined"
+                  color="primary"
+                  onClick={handleEditClick}
+                >
+                  <EditOutlinedIcon />
+                  Edit Profile
+                </Button>
+              </>
+            )}
+          </CardContent>
         </Card>
       </Box>
     </Box>
@@ -70,33 +188,37 @@ interface User {
   dob: string;
   mobile: string;
   email: string;
+  image?: string;
+  address?: string;
 }
 
 const cardStyle = {
+  display: "flex",
   p: 4,
-  width: 450,
-  borderRadius: 3,
-  textAlign: "center",
-  backgroundColor: "lightgray",
+  width: "70%",
+  maxWidth: 900,
+  borderRadius: 4,
+  backgroundColor: "white",
+  boxShadow: "0px 4px 20px rgba(0,0,0,0.1)",
 };
 
-const avatarStyle = {
-  bgcolor: "orange",
-  width: 80,
-  height: 80,
-  fontSize: "2rem",
-  mx: "auto",
-  mb: 2,
+const imageBoxStyle = {
+  flex: 1,
+  minWidth: 250,
+  maxWidth: 300,
+  height: 250,
+  borderRadius: "12px",
+  overflow: "hidden",
+  backgroundColor: "#f5f5f5",
+  marginRight: "30px",
 };
 
 const buttonStyle = {
-  mt: 2,
-  backgroundColor: "#0a2df2ff",
   textTransform: "none",
-  borderRadius: 8,
-  width: "60%",
-  px: 8,
-  py: 1.2,
+  borderRadius: 4,
+  gap: 2,
+  mt: 2,
+  px: 4,
   fontWeight: "bold",
   fontSize: "1rem",
 };
